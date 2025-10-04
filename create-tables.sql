5050c3bc7dee06dc96ea52ee4f5a62b79c2e1937
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2025-10-04 12:54:47 -0300

    feat: add ContaReceber table

--- create-tables.sql
+++ create-tables.sql
@@ -8,3 +8,15 @@ CREATE TABLE `Cliente` (
     PRIMARY KEY (`ID`),
     UNIQUE KEY `UK_Cliente_CPF` (`CPF`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `ContaReceber` (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `Cliente_ID` INT NOT NULL,
+    `FaturaVendaID` INT DEFAULT NULL,
+    `DataConta` DATE NOT NULL,
+    `DataVencimento` DATE NOT NULL,
+    `Valor` DECIMAL(18,2) NOT NULL,
+    `Situacao` ENUM('1','2','3') NOT NULL COMMENT '1=Registrada, 2=Cancelada, 3=Paga',
+    PRIMARY KEY (`ID`),
+    CONSTRAINT `fk_ContaReceber_Cliente` FOREIGN KEY (`Cliente_ID`) REFERENCES `Cliente` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
